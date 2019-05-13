73371f098ceece308277047b27e97d104741c7dd
Boris Belmar <borisbelmarm@gmail.com>
2019-05-13 15:28:41 -0400

    Avanzando en el Signup con passport

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
+const pool = require('../database');
+
+// Signup Routes
+
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+// Login Routes
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+// Profile Route
+
+router.get('/profile', (req, res) => {
+    res.send('This is your profile');
+});
+
 module.exports = router;
