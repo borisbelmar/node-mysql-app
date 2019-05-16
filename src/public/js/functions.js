07effcf1a1c36df15f1263283e0da68ffe318c34
Boris Belmar <borisbelmarm@gmail.com>
2019-05-16 15:34:09 -0400

    Cambios en la validación de formulario

--- src/public/js/functions.js
+++ src/public/js/functions.js
@@ -21,26 +21,59 @@ const closeMessage = () => {
 // Validadores
 // Optimizar con Childrens!
 
-let username = {
-    label: document.getElementById("usernameLabel"),
-    input: document.getElementById("usernameInput"),
-    help: document.getElementById("usernameHelp"),
-    validate: false
+const element = (id) => { return document.getElementById(id); };
+
+class inputFields {
+    constructor(elementId) {
+        this.id = elementId;
+        this.label = element(this.id).children[0];
+        this.input = element(this.id).children[1].children[0];
+        this.value = this.input.value;
+        this.help = element(this.id).children[1].children[1];
+        this.validate = false;
+    }
+}
+
+let nombre = new inputFields("nombre");
+
+// Un Experimento 
+
+const validateNombre = () => {
+    const validar = () => {
+        console.log(nombre.value);
+    };
+    const romper = () => {
+        nombre.input.removeEventListener("keypress");
+    };
+    nombre.input.addEventListener("keypress", validar);
+    nombre.input.addEventListener("onfocusout", romper);
 };
 
-let password = {
-    label: document.getElementById("usernameLabel"),
-    input: document.getElementById("usernameInput"),
-    help: document.getElementById("usernameHelp"),
-    validate: false
+nombre.input.addEventListener("focus", validateNombre);
+
+let apellido = new inputFields("apellido");
+
+const validateApellido = () => {
+    let validar = () => {
+        console.log(apellido.value);  
+    };
 };
 
-const validateUser = () => {
-    console.log(username.input.value);
+apellido.input.addEventListener("focusout", validateApellido);
+
+let username = new inputFields("username");
+
+const validateUsername = () => {
+    console.log(username.value);
 };
+
+username.input.addEventListener("focusout", validateUsername);
+
+let password = new inputFields("password");
+let repassword = new inputFields("repassword");
 
 const validatePassword = () => {
-
+    console.log(password.value);
 };
 
-username.input.addEventListener("focusout", validateUser);
+password.input.addEventListener("focusout", validatePassword);
